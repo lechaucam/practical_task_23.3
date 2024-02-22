344dfc30b1542948b261b445616145b328e9011f
lechaucam <nguyenlechaucam@hotmail.com>
2024-02-22 16:57:05 +0000

    comments modified

--- pattern.py
+++ pattern.py
@@ -1,7 +1,7 @@
-# create a for loop for 9 lines, start with 1 star and add a star on every row
-# if statement to stop adding stars after 5th row/loop
-# else statement to take away a star at each line starting with 4 stars on 6th row/loop
-# printing a smaller index as the loop goes on till the end
+# Create a for loop for 9 lines, start with 1 star and add a star on every row.
+# If statement to stop adding stars after 5th row/loop.
+# Else statement to take away a star at each line starting with 4 stars on 6th row/loop.
+# Printing a smaller index as the loop goes on till the end.
 
 star = "*"
 
